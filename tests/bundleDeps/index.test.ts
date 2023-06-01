f84b904d9a8c5bc7da376a23cac2cf905d85560d
changfeng <changfeng@gaoding.com>
2023-06-01 16:12:52 +0800

    test: execa cwd should be fixture folder

--- tests/bundleDeps/index.test.ts
+++ tests/bundleDeps/index.test.ts
@@ -5,7 +5,9 @@ import { execa } from 'execa';
 import { expect, test } from 'vitest';
 
 test('bundleDeps option', async () => {
-    await execa('pnpm', ['build']);
+    await execa('pnpm', ['build'], {
+        cwd: __dirname,
+    });
     const stat = await fs.stat(path.resolve(__dirname, './dist/bundle.esm.js'));
     const kbSize = stat.size / 1000;
     expect(kbSize > 275.95);
--- tests/depTypes/index.test.ts
+++ tests/depTypes/index.test.ts
@@ -5,7 +5,9 @@ import { execa } from 'execa';
 import { expect, test } from 'vitest';
 
 test('depTypes option', async () => {
-    await execa('pnpm', ['build']);
+    await execa('pnpm', ['build'], {
+        cwd: __dirname,
+    });
     const output = await fs.readFile(path.resolve(__dirname, './output.js'), 'utf8');
     const bundle = await fs.readFile(path.resolve(__dirname, './dist/bundle.esm.js'), 'utf8');
     expect(bundle).toBe(output);
